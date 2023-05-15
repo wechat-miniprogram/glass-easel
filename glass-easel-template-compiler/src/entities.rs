a13803120215ed28ba59311ae2d0b2564eeceb68
LastLeaf <bqfu@163.com>
2023-05-15 22:29:49 +0800

    feat: support HTML numeric chars (#33)

--- glass-easel-template-compiler/src/entities.rs
+++ glass-easel-template-compiler/src/entities.rs
@@ -1,7 +1,7 @@
 //! Helpers for parsing HTML entities
 
 use entities::ENTITIES;
-use std::collections::HashMap;
+use std::{collections::HashMap, borrow::Cow};
 
 lazy_static! {
     static ref ENTITIES_MAPPING: HashMap<&'static str, &'static str> = make_mapping();
@@ -15,9 +15,32 @@ fn make_mapping() -> HashMap<&'static str, &'static str> {
     mapping
 }
 
-pub(crate) fn decode(entity: &str) -> &str {
+pub(crate) fn decode<'a>(entity: &'a str) -> Cow<'a, str> {
+    dbg!(&entity);
+    let len = entity.len();
+    if &entity[(len - 1)..] != ";" {
+        return Cow::Borrowed(entity);
+    }
+    if len > 4 && &entity[1..=2] == "#x" {
+        let hex_str = &entity[3..(len - 1)];
+        if let Ok(hex) = u32::from_str_radix(hex_str, 16) {
+            if let Some(c) = char::from_u32(hex) {
+                return Cow::Owned(String::from(c));
+            }
+        }
+        return Cow::Borrowed(entity);
+    }
+    if len > 3 && &entity[1..=1] == "#" {
+        let digit_str = &entity[2..(len - 1)];
+        if let Ok(hex) = u32::from_str_radix(digit_str, 10) {
+            if let Some(c) = char::from_u32(hex) {
+                return Cow::Owned(String::from(c));
+            }
+        }
+        return Cow::Borrowed(entity);
+    }
     match ENTITIES_MAPPING.get(entity) {
-        None => entity,
-        Some(x) => *x,
+        None => Cow::Borrowed(entity),
+        Some(x) => Cow::Borrowed(*x),
     }
 }
