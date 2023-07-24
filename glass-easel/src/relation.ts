c8c936164dedc2b38c0bf51fadf947dbd7fee283
LastLeaf <bqfu@163.com>
2023-07-24 21:15:17 +0800

    fix: use closest parent/child pair in ancestor/descendant ralations (#69)

--- glass-easel/src/relation.ts
+++ glass-easel/src/relation.ts
@@ -225,6 +225,7 @@ export class Relation {
                         break
                       }
                     }
+                    if (newLink) break
                   }
                 }
               }
