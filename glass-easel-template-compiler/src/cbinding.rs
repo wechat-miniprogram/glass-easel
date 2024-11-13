d68aeed0892c3ecb41453d5c3784a3eae4a7de37
SgLy <775150558@qq.com>
2024-11-13 17:27:49 +0800

    feat(template-compiler): add stringify_tmpl for cbindgen

--- glass-easel-template-compiler/src/cbinding.rs
+++ glass-easel-template-compiler/src/cbinding.rs
@@ -365,6 +365,12 @@ impl TmplGroup {
     pub unsafe extern "C" fn tmpl_group_export_all_scripts(&self) -> StrRef {
         self.inner().export_all_scripts().unwrap_or_default().into()
     }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn tmpl_group_stringify_tmpl(&self, path_buf: &u8, path_len: usize) -> StrRef {
+        let path = String::from_utf8_lossy(slice::from_raw_parts(path_buf, path_len));
+        self.inner().stringify_tmpl(&path).unwrap_or_default().into()
+    }
 }
 
 impl Drop for TmplGroup {
