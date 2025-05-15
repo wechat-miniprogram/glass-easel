364b044ee44c3dc07b04a48d6cc1dd4837068bd9
chyizheng <chyizheng@tencent.com>
2025-05-15 13:30:54 +0800

    feat: add dropBackendAfterRelease for backend context

--- glass-easel/tests/base/env.ts
+++ glass-easel/tests/base/env.ts
@@ -124,7 +124,9 @@ export const multiTmpl = (src: { [path: string]: string }, options?: TemplateOpt
   }
 }
 
-export const domBackend = new glassEasel.CurrentWindowBackendContext()
+export const domBackend = new class extends glassEasel.CurrentWindowBackendContext {
+  public dropBackendAfterRelease = true
+}
 export const shadowBackend = new ShadowBackend.Context()
 export const composedBackend = new ComposedBackend.Context()
 
