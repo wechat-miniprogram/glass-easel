7702ad2ef6151d7889cbd57b64ed262e9330a741
chyizheng <chyizheng@tencent.com>
2026-02-05 17:24:41 +0800

    chore: change npm package structure for compilers

--- glass-easel-stylesheet-compiler/main.js
+++ glass-easel-stylesheet-compiler/main.js
@@ -1,14 +1,14 @@
 const path = require('node:path')
 const fs = require('node:fs')
-const bg = require('./pkg/glass_easel_template_compiler_bg.cjs.js')
+const bg = require('./pkg/glass_easel_stylesheet_compiler_bg.cjs.js')
 
 const bytes = fs.readFileSync(
-  path.resolve(__dirname, './pkg/glass_easel_template_compiler_bg.wasm'),
+  path.resolve(__dirname, './pkg/glass_easel_stylesheet_compiler_bg.wasm'),
 )
 
 const wasmModule = new WebAssembly.Module(bytes)
 const wasmInstance = new WebAssembly.Instance(wasmModule, {
-  './glass_easel_template_compiler_bg.js': bg,
+  './glass_easel_stylesheet_compiler_bg.js': bg,
 })
 
 bg.__wbg_set_wasm(wasmInstance.exports)
